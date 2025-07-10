c23f3ba8bec0177b38be02ba18139df709c569eb
Mel Massadian <mel@melmassadian.com>
2025-07-10 23:48:53 +0200

    chore: 🧹 bump mengine version

--- engine/mmengine/version.py
+++ engine/mmengine/version.py
@@ -6,7 +6,9 @@
 
 
 # __version__ = '0.8.4' ## forked mmengine version
-__version__ = '1.0.0'
+# __version__ = '1.0.0' ## forked sapiens-engine version
+__version__ = "1.0.1"
+
 
 def parse_version_info(version_str):
     """Parse the version information.
@@ -18,13 +20,13 @@ def parse_version_info(version_str):
         tuple: version information contains major, minor, micro version.
     """
     version_info = []
-    for x in version_str.split('.'):
+    for x in version_str.split("."):
         if x.isdigit():
             version_info.append(int(x))
-        elif x.find('rc') != -1:
-            patch_version = x.split('rc')
+        elif x.find("rc") != -1:
+            patch_version = x.split("rc")
             version_info.append(int(patch_version[0]))
-            version_info.append(f'rc{patch_version[1]}')
+            version_info.append(f"rc{patch_version[1]}")
     return tuple(version_info)
 
 
